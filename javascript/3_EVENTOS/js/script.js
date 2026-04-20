8cc63e82bb974bc2ff28d388afd08273e0d4c41c
Ray Gonçalves <raaycunha.dev@gmail.com>
2026-04-20 20:35:48 -0300

    Eventos

---
+++ javascript/3_EVENTOS/js/script.js
@@ -0,0 +1,19 @@
+// 1 - debounce
+
+const debounce = (f, delay) => {
+    let timeout
+
+    return (...arguments) => {
+        if (timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        }, delay)
+    }
+}
+
+window.addEventListener('mousemove', debounce(() => {
+    console.log('Executando a cada 400ms')
+}, 400))
